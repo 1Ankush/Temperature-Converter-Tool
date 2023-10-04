3ca8e85e97571724a0276a87b08e7d73d05a9fb3
1Ankush <146944712+1Ankush@users.noreply.github.com>
2023-10-04 17:50:12 +0530

    Add files via upload

---
+++ Mymain.java
@@ -0,0 +1,84 @@
+package com.internshala.javafx.Mymain;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.util.Optional;
+
+public class Mymain extends Application{
+public static void main(String[] args){
+launch(args);
+}
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		FXMLLoader loader = new
+				FXMLLoader(getClass().getResource("app_layout.fxml"));
+		VBox rootNode = loader.load();
+
+		MenuBar menuBar =  createMenu();
+		rootNode.getChildren().add(0,menuBar);
+		Scene scene = new Scene(rootNode);
+
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Temperature Converter Tool ");
+		primaryStage.show();
+	}
+	private  MenuBar createMenu(){
+	 // file Menu
+		Menu fileMenu = new Menu("File");
+		MenuItem newMenuItem = new MenuItem("New");
+
+		newMenuItem.setOnAction(event -> System.out.println("New Menu Item clicked"));
+
+
+		SeparatorMenuItem separatorMenuItem = new SeparatorMenuItem();
+
+		MenuItem quitMenuItem = new MenuItem("Quit");
+		quitMenuItem.setOnAction(event -> {
+			Platform.exit();
+			System.exit(0);
+		});
+		fileMenu.getItems().addAll(newMenuItem, separatorMenuItem,quitMenuItem);
+
+		//Help Menu
+		Menu helpMenu = new Menu("Help");
+		MenuItem aboutapp = new MenuItem("About");
+
+		aboutapp.setOnAction(event -> aboutapp());
+		helpMenu.getItems().addAll(aboutapp);
+
+		MenuBar menuBar = new MenuBar();
+		menuBar.getMenus().addAll(fileMenu,helpMenu);
+		return menuBar;
+	}
+
+	private void aboutapp() {
+	// to do
+		Alert alertDialog = new Alert(Alert.AlertType.INFORMATION);
+		alertDialog.setTitle("My First Desktop App");
+		alertDialog.setHeaderText("learning JavaFX");
+		alertDialog.setContentText("I am just a beginner but soon I will be pro and start developing Amazing Java Games");
+		ButtonType yesBtn = new ButtonType("Yes");
+		ButtonType noBtn = new ButtonType("No");
+
+		alertDialog.getButtonTypes().setAll(yesBtn,noBtn);
+		Optional<ButtonType> clilckedBtn = alertDialog.showAndWait();
+
+		if(clilckedBtn.isPresent() && clilckedBtn.get() == yesBtn){
+			System.out.println("Yes Button clicked");
+		}
+		else
+		{
+			System.out.println("No Button clicked");
+		}
+	}
+}
